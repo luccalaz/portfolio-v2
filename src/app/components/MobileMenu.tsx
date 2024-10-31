c8f466dd61124decbb8504be2bdc2719ce0e5269
luccalaz <luccalazza@gmail.com>
2024-10-30 23:56:05 -0300

    Fix mobile menu animation

--- src/app/components/MobileMenu.tsx
+++ src/app/components/MobileMenu.tsx
@@ -6,7 +6,7 @@ interface MobileMenuProps {
   
   const MobileMenu: React.FC<MobileMenuProps> = ({activeSection, showMobileMenu, setShowMobileMenu}) => {
     return (
-      <nav id="mobile" className={`fixed top-16 z-40 w-screen h-screen bg-zinc-900 text-zinc-400 border-b border-b-zinc-800 ${showMobileMenu && "show"}`}>
+      <nav id="mobile" className={`fixed z-40 w-screen h-screen bg-zinc-900 text-zinc-400 ${showMobileMenu && "show"}`}>
         <div className="mobile-menu">
           <a href="#projects" className={`m-navlink ${activeSection === "projects" && "active"}`} onClick={() => setShowMobileMenu(!showMobileMenu)}> Projects</a>
           <a href="#resume" className={`m-navlink ${activeSection === "resume" && "active"}`} onClick={() => setShowMobileMenu(!showMobileMenu)}>Resume</a>
